95e0b0d9348b2240c1dd4db19f22356f5d4cfb27
Nikita Kondla <nkondla@Nikita-Mac.attlocal.net>
2020-08-23 19:12:05 -0400

    added HelloNikita.java

---
+++ HelloNikita.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package java_project1;
+
+/**
+ * @author nkondla
+ *
+ */
+public class HelloNikita {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+
+	}
+
+}
